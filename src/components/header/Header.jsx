cfc51c7ec465b71e2237e9421f6241d3cc3b536c
shahzadakram786 <alyakram786@gmail.com>
2024-09-11 20:44:14 +0500

    header styling and cart page has been done only ui

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,18 +5,33 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <Box className="container" sx={{ background: "aqua" }}>
+      <Box
+        className="container"
+        sx={{
+          padding: "2px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          // backgroundColor: "red",
+        }}
+      >
         <Box
           className="mainContainer"
           sx={{
             display: "flex",
-            justifyContent: "space-around",
+            justifyContent: "space-between",
             alignItems: "center",
             padding: "20px",
+            // background: "yellow",
+            width: "80%",
+            borderBottom: "1px solid white",
           }}
         >
           <Box>
-            <Typography sx={{ fontSize: "20px" }} variant="h1">
+            <Typography
+              sx={{ fontSize: "25px", color: "aqua", cursor: "pointer" }}
+              variant="h1"
+            >
               Unsplash Images
             </Typography>
           </Box>
--- src/page/cart/Cart.jsx
+++ src/page/cart/Cart.jsx
@@ -1,7 +1,34 @@
+import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const Cart = () => {
-  return <div>This is carts area</div>;
+  return (
+    <Box
+      sx={{
+        // background: "yellow",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        padding: "2.5rem 5rem",
+        minHeight: "77vh",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          // background: "red",
+        }}
+      >
+        <Typography varient="h1" sx={{ color: "white", fontSize: "40px" }}>
+          NO ANY CART SELECTED
+        </Typography>
+      </Box>
+    </Box>
+  );
 };
 
 export default Cart;
